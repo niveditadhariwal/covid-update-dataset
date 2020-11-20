3ba085f0199eab2ac2ce1d94de26fd94f402b107
Nivedita Dhariwal <65903746+niveditadhariwal@users.noreply.github.com>
2020-11-20 20:49:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import requests
-from plotly.offline import iplot
 import plotly.graph_objs as go
 import plotly.express as px
 from pandas.io.json import json_normalize
